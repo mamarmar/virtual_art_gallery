9ecc300a272ca912f58c03fb14c29f076629a079
Margarita Marmaridou <margarita.marmaridou@simplify.art>
2024-02-13 06:43:16 +0000

    navbar styling completed

--- src/components/modules/NavigationBar.jsx
+++ src/components/modules/NavigationBar.jsx
@@ -1,42 +1,49 @@
 import { useState } from "react";
-import Box from "@mui/material/Box";
 import {
+  Box,
   IconButton,
-  Button,
   Typography,
   Menu,
   MenuItem,
   AppBar,
   Stack,
+  Toolbar,
 } from "@mui/material";
 import { AccountCircle } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import { useStore } from "../../store/index";
 import { supabase } from "../../supabaseClient";
-import User from "feather-icons";
 
 const containerStyle = {
-  position: "fixed",
-  top: 0,
-  right: 0,
-  left: 0,
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
+  boxShadow: "none",
   borderBottom: "2.5px solid #F9F9F9",
-  padding: "24px",
   backgroundColor: "white",
-  zIndex: "100",
 };
 
 const profileButtonStyle = {
   color: "black",
+  padding: 0,
   "&:focus": {
     outline: "none",
+    backgroundColor: "transparent",
   },
   "&:hover": {
     backgroundColor: "white",
   },
+};
+
+const menuStyle = {
+  mt: "3em",
+  position: "absolute",
+  right: "1.5em",
+  "& .MuiList-root": {
+    padding: 0,
+  },
+};
+
+const menuItemStyle = {
+  fontFamily: "Poppins",
+  padding: "8px",
 };
 
 const NavigationBar = () => {
@@ -84,48 +91,54 @@ const NavigationBar = () => {
   };
 
   return (
-    <Box sx={containerStyle}>
-      <p className="logo" onClick={() => navigate("/")}>
-        Visual Art Gallery
-      </p>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={containerStyle}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <p className="logo" onClick={() => navigate("/")}>
+            Visual Art Gallery
+          </p>
 
-      {!isUser && (
-        <Stack direction="row" spacing="0.6rem">
-          <button className="secondary" onClick={() => navigate("/login")}>
-            Log in
-          </button>
-          <button className="primary" onClick={() => navigate("/signup")}>
-            Sign up
-          </button>
-        </Stack>
-      )}
-      {isUser && (
-        <IconButton onClick={handleOpenUserMenu} sx={profileButtonStyle}>
-          <AccountCircle sx={{ fontSize: "1.5em" }} />
-        </IconButton>
-      )}
-      <Menu
-        sx={{ mt: "3em", position:"absolute", right:"1.5em"}}
-        id="menu-appbar"
-        anchorEl={anchorElUser}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        open={Boolean(anchorElUser)}
-        onClose={handleCloseUserMenu}
-      >
-        {settings.map((setting) => (
-          <MenuItem key={setting.name} onClick={setting.action}>
-            <Typography textAlign="center">{setting.name}</Typography>
-          </MenuItem>
-        ))}
-      </Menu>
+          {!isUser && (
+            <Stack direction="row" spacing="0.6rem">
+              <button className="secondary" onClick={() => navigate("/login")}>
+                Log in
+              </button>
+              <button className="primary" onClick={() => navigate("/signup")}>
+                Sign up
+              </button>
+            </Stack>
+          )}
+          {isUser && (
+            <IconButton onClick={handleOpenUserMenu} sx={profileButtonStyle}>
+              <AccountCircle sx={{ fontSize: "1.5em" }} />
+            </IconButton>
+          )}
+          <Menu
+            sx={menuStyle}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting.name} onClick={setting.action}>
+                <Typography textAlign="center" sx={menuItemStyle}>
+                  {setting.name}
+                </Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Toolbar>
+      </AppBar>
     </Box>
   );
 };
