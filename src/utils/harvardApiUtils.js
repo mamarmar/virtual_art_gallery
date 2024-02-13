564af9822e126b2184ccd363c65eed26bf328b53
Margarita Marmaridou <margarita.marmaridou@simplify.art>
2024-02-13 12:07:45 +0000

    issue with year fixed

--- src/utils/harvardApiUtils.js
+++ src/utils/harvardApiUtils.js
@@ -11,6 +11,8 @@ export const fetchArtworks = async (params) => {
       data: { info, records },
     } = response;
 
+    console.log("RECORDS", records);
+
     // Keep only the artwork information needed
     const receivedArtworks = records.map((artwork) => {
       // Find the artist if they exist
@@ -42,10 +44,7 @@ export const fetchArtworks = async (params) => {
         id: artwork.id,
         title: artwork.title,
         artist,
-        year:
-          artwork.datebegin === artwork.dateend
-            ? artwork.datebegin
-            : `${artwork.datebegin}-${artwork.dateend}`,
+        year: artwork.dated ?? "Unknown year",
         culture: artwork.culture,
         medium: artwork.medium,
         division: artwork.division,
@@ -60,15 +59,15 @@ export const fetchArtworks = async (params) => {
 };
 
 export const fetchArtists = async (params) => {
-    try {
-        const response = await axios.get(
-            `${import.meta.env.VITE_HARVARD_MUSEUMS_API}/person?${params}&apikey=${
-              import.meta.env.VITE_HARVARD_MUSEUMS_API_KEY
-            }`
-          );
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_HARVARD_MUSEUMS_API}/person?${params}&apikey=${
+        import.meta.env.VITE_HARVARD_MUSEUMS_API_KEY
+      }`
+    );
 
-          console.log("RESPONSE_ARTISTS", response)
-    } catch (error) {
-        console.log("ARTISTS_ERROR", error)
-    }
-}
+    console.log("RESPONSE_ARTISTS", response);
+  } catch (error) {
+    console.log("ARTISTS_ERROR", error);
+  }
+};
